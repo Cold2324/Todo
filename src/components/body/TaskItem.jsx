63eb6b62f3f5a7957521a5f65a1c178584f5cc6e
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-03-21 15:06:56 -0500

    Body style added

--- src/components/body/TaskItem.jsx
+++ src/components/body/TaskItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import deleteIcon from '../../assets/icons/delete.svg'
+import check from '../../assets/icons/check.svg'
 
 function TaskItem({ task }) {
   return (
     <div className="body--task-item">
-      <span>{task.text}</span>
+      <p className="body--task-content">
+        <img src={check} className="body--task-icon-check" />
+        <span>{task.text}</span>
+        <img src={deleteIcon} className="body--task-icon-delete" />
+      </p>
     </div>
   )
 }
