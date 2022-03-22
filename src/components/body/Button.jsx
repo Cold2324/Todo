e013ac38803ca7cceb6acc182747778ac603288c
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-03-21 22:46:04 -0500

    "button component created and button styles added"

---
+++ src/components/body/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../sass/body/button.scss'
+
+function Button() {
+  return (
+    <div className="button--container">
+      <button>+</button>
+    </div>
+  )
+}
+
+export default Button
