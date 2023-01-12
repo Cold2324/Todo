582091aa26b25be7be571fa4d1ba0657ece124c2
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2023-01-12 13:44:04 -0500

    refactor: eslint and prettier added; change version of react and start vite configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './sass/index.scss'
 
-ReactDOM.render(
+ReactDOM.createRoot(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
