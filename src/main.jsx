3b2d31e964b9d3714b556d9c42f4481b4d6381d8
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2023-01-12 13:57:44 -0500

    refactor: vite config and fix some errors

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './sass/index.scss'
 
-ReactDOM.createRoot(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
